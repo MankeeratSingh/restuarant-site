a9031dc0ee517c0a308478ee0862d32cd88b2786
MankeeratSingh <40733993+MankeeratSingh@users.noreply.github.com>
2018-07-10 14:49:37 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ var allCategoriesUrl =
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
 var menuItemsUrl =
-  "https://mankeeratsingh.github.io/MItems/b.json";
+  "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
 var menuItemsTitleHtml = "snippets/menu-items-title.html";
 var menuItemHtml = "snippets/menu-item.html";
 
